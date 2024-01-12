56c78dc5709b910493f5dec92d651642f0144337
mac <mac@macdeMacBook-Pro.local>
2024-01-12 21:12:17 +0800

    add train

---
+++ train.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+# 加载一个模型
+model = YOLO('yolov8s.yaml')  # 从YAML建立一个新模型
+
+# 训练模型
+# results = model.train(data='coco128.yaml', epochs=100, imgsz=640)
+results = model.train(data='xray.yaml', epochs=100, imgsz=640, device=[0, 1])
