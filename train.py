e43ba21b1a936dfc86158eb1d37eb32992e05381
mac <mac@macdeMacBook-Pro.local>
2024-01-15 19:34:22 +0800

    add eval

--- train.py
+++ train.py
@@ -5,5 +5,5 @@ model = YOLO('yolov8x.yaml')  # 从YAML建立一个新模型
 
 # 训练模型
 # results = model.train(data='coco128.yaml', epochs=100, imgsz=640)
-results = model.train(data='xray.yaml', epochs=100, imgsz=640, device=[0, 1])
+results = model.train(data='xray.yaml', epochs=100, imgsz=640, device=[1, 2])
 metrics = model.val()
