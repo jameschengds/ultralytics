7653a8c651fe91341942782f287c91596353e59f
mac <mac@macdeMacBook-Pro.local>
2024-01-16 15:52:28 +0800

    change pt

--- train.py
+++ train.py
@@ -2,11 +2,10 @@ from ultralytics import YOLO
 from ultralytics import settings
 
 # 加载一个模型
-model = YOLO('yolov8x.yaml')  # 从YAML建立一个新模型
+model = YOLO('yolov8x.pt')  # 从YAML建立一个新模型
 #
 # settings.update({'runs_dir': '/mnt/dgx/ultralytics/runs', "datasets_dir": "/mnt/yolo_data",
 #                  "weights_dir": "/mnt/dgx/ultralytics/weights"})
 # 训练模型
 # results = model.train(data='coco128.yaml', epochs=100, imgsz=640)
 results = model.train(data='xray.yaml', epochs=100, imgsz=640, device=[0, 1])
-metrics = model.val()
